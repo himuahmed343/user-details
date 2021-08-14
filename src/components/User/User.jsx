48032926bc03d57c7a1a23cae18dfa83e837f718
himubhai20 <himuahmed343@gmail.com>
2021-08-14 16:08:42 +0600

    change user components

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './User.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlusSquare } from '@fortawesome/free-solid-svg-icons'
+import { faPlusSquare } from '@fortawesome/free-solid-svg-icons';
+
 
 
 const User = (props) => {
-    const {name, email, salary} = props.user;
-    const eventHandle = props.eventHandle
+    const {name, email, salary, phone} = props.user;
+    const eventHandle = props.eventHandle;
+    const [mobile, setMobile] = useState('');
+
+
+    const showPhone = () => setMobile(phone);
     // console.log(props.user)
     return (
         <div className="user-box">
             <p>This is <b>{name}</b></p>
             <p>Email: {email}</p>
             <p>Salary: {salary}</p>
+            <p>Phone: {mobile}</p>
             <button className= 'main-btn' onClick = {() => eventHandle(props.user)}>Add<FontAwesomeIcon className = 'plus-icon' icon={faPlusSquare} /></button>
+            <button className= 'main-btn' onClick={showPhone}>Show Phone Number</button>
+            
         </div>
     );
 };
